0302fbbf22976526daeda7f8b2339d02b273791f
Ian Glow <ianglow@usc.edu>
2014-12-07 05:38:17 -0800

    I better get an A++++

--- IanGlow_CSCI201_Assignment5b/src/edu/usc/ianglow/server/ToolShead.java
+++ IanGlow_CSCI201_Assignment5b/src/edu/usc/ianglow/server/ToolShead.java
@@ -1,29 +1,32 @@
 package edu.usc.ianglow.server;
 
 import java.util.Vector;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class ToolShead {
-	
+
 	public static int SCREWDRIVER = 0,
 			HAMMER = 1,
 			PAINTBRUSH = 2,
 			PLIERS = 3,
 			SCISSORS = 4;
-	
+
 	Object inputLock = new Object();
 	Object outputLock = new Object();
 	Object waitLock = new Object();
-	
+
+	ReentrantLock tryLock = new ReentrantLock();
+
 	MainFrame frame;
-	
+
 	int num_screw, num_hammer, num_pb, num_pliers, num_scissors,
-		num_screwt, num_hammert, num_pbt, num_plierst, num_scissorst;
-	
+	num_screwt, num_hammert, num_pbt, num_plierst, num_scissorst;
+
 	public ToolShead(MainFrame frame)
 	{
 		this.frame = frame;
 	}
-	
+
 	public void init() {
 		num_screw = num_screwt;
 		num_hammer = num_hammert;
@@ -39,20 +42,21 @@ public class ToolShead {
 		frame.paintbrush.label = "" + num_pb + "/" + num_pbt;
 		frame.pliers.label = "" + num_pliers + "/" + num_plierst;
 		frame.scissors.label = "" + num_scissors + "/" + num_scissorst;
-		
+
 		frame.repaint();
 	}
-	
+
 	public void getTools(Worker w, LarpListener l, Action a){
-//		System.out.println("Get tools, outside of lock1");
+		//		System.out.println("Get tools, outside of lock1");
+
 		synchronized(outputLock){
-//			System.out.println("Get tools, outside of lock2");
+			//			System.out.println("Get tools, outside of lock2");
 			while(!enoughTools(a.tools))
 			{
-//				System.out.println("!Enough tools");
+				//				System.out.println("!Enough tools");
 				synchronized(waitLock)
 				{
-//					System.out.println("waiting");
+					//					System.out.println("waiting");
 					try {
 						waitLock.wait();
 					} catch (InterruptedException e) {
@@ -60,12 +64,16 @@ public class ToolShead {
 					}
 				}
 			}
-			
+
+			tryLock.lock();
 			w.getTools(); //Does not returen untill done
+			tryLock.unlock();
 		}
+
 		l.reachedLocation();
+
 	}
-	
+
 	public void takeTool(int i)
 	{
 		synchronized(inputLock)//before actual take
@@ -73,7 +81,7 @@ public class ToolShead {
 			if(i == SCREWDRIVER)
 				num_screw--;
 			else if(i == HAMMER)
-				 num_hammer--;
+				num_hammer--;
 			else if(i == PLIERS)
 				num_pliers--;
 			else if(i == SCISSORS)
@@ -81,13 +89,13 @@ public class ToolShead {
 			else
 				num_pb--;
 		}
-		
+
 		update();
 	}
-	
+
 	public void returnTool(final Worker w, final LarpListener l, final int tool){
 		System.out.println("returnTool");
-		
+
 		w.facneyLarp(new LarpListener(){
 			@Override
 			public void reachedLocation() {
@@ -98,17 +106,17 @@ public class ToolShead {
 					w.removeTool(tool);
 					l.reachedLocation();
 					System.out.println("Before Lock2");
-					
+
 					synchronized(waitLock)
 					{
 						System.out.println("Tool Returned");
 						waitLock.notifyAll();
 					}
 				}
-				
+
 			}}, getAssociatedSqure(tool));
 	}
-	
+
 	public Square getAssociatedSqure(int tool)
 	{
 		if(tool == SCREWDRIVER)
@@ -119,10 +127,10 @@ public class ToolShead {
 			return frame.pliers;
 		if(tool == SCISSORS)
 			return frame.scissors;
-		
+
 		return frame.paintbrush;
 	}
-	
+
 	public void createTool(int tool)
 	{
 		addTool(tool);
@@ -132,13 +140,13 @@ public class ToolShead {
 			waitLock.notifyAll();
 		}
 	}
-	
+
 	public void addTool(int tool)
 	{
 		if(tool == SCREWDRIVER)
 			num_screw++;
 		else if(tool == HAMMER)
-			 num_hammer++;
+			num_hammer++;
 		else if(tool == PLIERS)
 			num_pliers++;
 		else if(tool == SCISSORS)
@@ -147,13 +155,13 @@ public class ToolShead {
 			num_pb++;
 		update();
 	}
-	
+
 	public void removeTool(int tool)
 	{
 		if(tool == SCREWDRIVER)
 			num_screw--;
 		else if(tool == HAMMER)
-			 num_hammer--;
+			num_hammer--;
 		else if(tool == PLIERS)
 			num_pliers--;
 		else if(tool == SCISSORS)
@@ -161,17 +169,17 @@ public class ToolShead {
 		else
 			num_pb--;
 	}
-	
+
 	public boolean enoughTools(Vector<Integer> tools)
 	{
 		int num_screw = 0, num_hammer = 0, num_pb = 0, num_pliers = 0, num_scissors = 0;
-		
+
 		for(Integer i : tools)
 		{
 			if(i == SCREWDRIVER)
 				num_screw++;
 			else if(i == HAMMER)
-				 num_hammer++;
+				num_hammer++;
 			else if(i == PLIERS)
 				num_pliers++;
 			else if(i == SCISSORS)
@@ -179,7 +187,7 @@ public class ToolShead {
 			else
 				num_pb++;
 		}
-		
+
 		if(num_screw > this.num_screw)
 			return false;
 		if(num_hammer > this.num_hammer)
@@ -190,46 +198,57 @@ public class ToolShead {
 			return false;
 		if(num_pb > this.num_pb)
 			return false;
-		
+
 		return true;
 	}
 
 	public boolean deleteTool(int tool) {
-		synchronized(outputLock){
-			if(tool == SCREWDRIVER && 0 < this.num_screw)
-			{
-				num_screw--;
-				num_screwt--;
-				return true;
-			}
-			else if(tool == HAMMER && 0 < this.num_hammer)
-			{
-				 num_hammer--;
-				 num_hammert--;
-				 return true;
-			}
-			else if(tool == PLIERS && 0 < this.num_pliers)
-			{
-				num_pliers--;
-				num_plierst--;
-				return true;
-			}
-			else if(tool == SCISSORS && 0 < this.num_scissors)
-			{
-				num_scissors--;
-				num_scissorst--;
-				return true;
-			}
-			else if(tool == PAINTBRUSH && 0 < this.num_pb)
-			{
-				num_pb--;
-				num_pbt--;
-				return true;
-			}
-			else
-				return false;
+
+		if(!tryLock.tryLock())
+			return false;
+
+		if(tool == SCREWDRIVER && 0 < this.num_screw)
+		{
+			num_screw--;
+			num_screwt--;
+			tryLock.unlock();
+			return true;
 		}
+		else if(tool == HAMMER && 0 < this.num_hammer)
+		{
+			num_hammer--;
+			num_hammert--;
+			tryLock.unlock();
+			return true;
+		}
+		else if(tool == PLIERS && 0 < this.num_pliers)
+		{
+			num_pliers--;
+			num_plierst--;
+			tryLock.unlock();
+			return true;
+		}
+		else if(tool == SCISSORS && 0 < this.num_scissors)
+		{
+			num_scissors--;
+			num_scissorst--;
+			tryLock.unlock();
+			return true;
+		}
+		else if(tool == PAINTBRUSH && 0 < this.num_pb)
+		{
+			num_pb--;
+			num_pbt--;
+			tryLock.unlock();
+			return true;
+		}
+		else
+		{
+			tryLock.unlock();
+			return false;
+		}
+
 	}
-	
+
 
 }
